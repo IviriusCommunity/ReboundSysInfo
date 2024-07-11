004e858d870b1f8cd082a6d20792f2eaafab2cf2
ErrorTek <jakub.baranski.33@gmail.com>
2024-07-11 20:06:57 +0100

    Implement CPU, RAM, and Windows version information

--- ReboundSysInfo/Views/SystemInformationPage.xaml.cs
+++ ReboundSysInfo/Views/SystemInformationPage.xaml.cs
@@ -11,8 +11,10 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Win32;
+using System.Management;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using System.Reflection.Metadata.Ecma335;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,6 +33,45 @@ public sealed partial class SystemInformationPage : Page
         wallBox.UriSource = new Uri(GetCurrentWallpaper());
         deviceName.Text = (string)model;
         manufacturerName.Text = (string)manufacturer;
+        DeviceName.Text = (string)model + $" ({Environment.MachineName})";
+        cpu.Text = GetCPUName();
+        ram.Text = GetRAMAmount();
+        WindowsVersion.Text = Environment.OSVersion.ToString();
+    }
+
+    public static string GetCPUName() {
+        var cpu =
+    new ManagementObjectSearcher("select * from Win32_Processor")
+    .Get()
+    .Cast<ManagementObject>()
+    .First();
+        var ProcessorName = (string)cpu["Name"];
+
+        ProcessorName =
+           ProcessorName
+           .Replace("(TM)", "™")
+           .Replace("(tm)", "™")
+           .Replace("(R)", "®")
+           .Replace("(r)", "®")
+           .Replace("(C)", "©")
+           .Replace("(c)", "©")
+           .Replace("    ", " ")
+           .Replace("  ", " ");
+        return ProcessorName;
+    }
+
+    public string GetRAMAmount()
+    {
+
+        ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+        ManagementObjectSearcher managementObjectSearcher = new ManagementObjectSearcher(objectQuery);
+        ManagementObjectCollection managementObjectCollection = managementObjectSearcher.Get();
+        var amount = "";
+        foreach (ManagementObject managementObject in managementObjectCollection)
+        {
+            amount = $"{managementObject["TotalVisibleMemorySize"]}MB RAM";
+        }
+        return amount;
     }
 
     private string GetCurrentWallpaper()
